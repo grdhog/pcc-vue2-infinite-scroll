a0227cd789afb2b3ca57d174bb0ec931b18fff49
grdhog <grdhog@users.noreply.github.com>
2023-01-13 10:05:07 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pcc-vue2-infinite-scroll
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vite-starter-vger7h)
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+  
+  </div>
+</template>
+
+<style>
+
+</style>
